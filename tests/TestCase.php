c726550bbf63e9dd0f93d971b924cdb0bac5c662
USAMI Kenta <tadsan@zonu.me>
2019-10-19 18:31:22 +0900

    Add PHPUnit

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace zonuexe\Zonufuck;
+
+/**
+ * Base class for zonuexe\Zonufuck test cases
+ *
+ * @copyright 2019 zonuexe
+ * @author USAMI Kenta <tadsan@zonu.me>
+ * @license GPL-3.0-or-later
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * bootstrap script for testcase
+ *
+ * @copyright 2019 zonuexe
+ * @author USAMI Kenta <tadsan@zonu.me>
+ * @license GPL-3.0-or-later
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
